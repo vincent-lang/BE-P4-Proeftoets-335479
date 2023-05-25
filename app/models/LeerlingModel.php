beb82473e9ac11a54f030e298f8973321f00a082
vincent van de merwe <335479@student.mboutrecht.nl>
2023-05-25 14:25:19 +0200

    6e commit

--- app/models/LeerlingModel.php
+++ app/models/LeerlingModel.php
@@ -1,27 +1,12 @@
 <?php
 
-class InstructeurModel
+class LeerlingModel
 {
     private $db;
 
     public function __construct()
     {
         $this->db = new Database();
-    }
-
-    public function getInstructeurs()
-    {
-        $sql = "SELECT Id
-                       ,Voornaam
-                       ,Tussenvoegsel
-                       ,Achternaam
-                       ,Mobiel
-                       ,DatumInDienst
-                       ,AantalSterren
-                FROM   Instructeur ORDER BY AantalSterren DESC";
-
-        $this->db->query($sql);
-        return $this->db->resultSet();
     }
 
     public function getLeerlingen($Id = null)
